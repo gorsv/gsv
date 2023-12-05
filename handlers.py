5c80347737f7c75eb8a7da7ea29140e95530b4b4
gorsv <gorbulin.sv@gmail.com>
2023-12-06 01:44:55 +0300

    3

--- handlers.py
+++ handlers.py
@@ -9,9 +9,11 @@ router = Router()
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
+    await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu)
 
 
-@router.message()
-async def message_handler(msg: Message):
-    await msg.answer(f"Твой ID: {msg.from_user.id}")
+@router.message(F.text == "Меню")
+@router.message(F.text == "Выйти в меню")
+@router.message(F.text == "◀️ Выйти в меню")
+async def menu(msg: Message):
+    await msg.answer(text.menu, reply_markup=kb.menu)
