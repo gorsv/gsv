80b1843a3224433f4445f5169a9800b1a1183aeb
gorsv <gorbulin.sv@gmail.com>
2023-12-05 22:07:07 +0300

    1

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 import config
+
 from handlers import router 
 
 
 async def main():
     bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
-    dp = Dispacher(storage=MemoryStorage())
+    dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_types())
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == "__main__" :
     logging.basicConfig(level=logging.INFO)
