99b31f00b37d3d4a4ce13ad42155bf3a3d9e74a3
gorsv <gorbulin.sv@gmail.com>
2023-12-21 21:47:40 +0300

    /

--- kb.py
+++ kb.py
@@ -1,9 +1,9 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 menu = [
     [InlineKeyboardButton(text="Спросить ИИ", callback_data="generate_text"),
-    InlineKeyboardButton(text="Узнать Расписание", callback_data="")],
-    [InlineKeyboardButton(text="Спросить ИИ", callback_data=""),
-    InlineKeyboardButton(text="Узнать Расписание", callback_data="")],
+    InlineKeyboardButton(text="Узнать Расписание", callback_data="generate_rasp")],
+    [InlineKeyboardButton(text="Краткое содержание", callback_data="generate_recenz"),
+    InlineKeyboardButton(text="Краткая информация по предмету", callback_data="generate_shpora")],
     [InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
 ]
 menu = InlineKeyboardMarkup(inline_keyboard=menu)
