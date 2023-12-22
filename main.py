ef1a24d00f2ccf50eee73c89fe095263d85b8de7
FlooGeR31 <82161469+FlooGeR31@users.noreply.github.com>
2023-12-22 19:07:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-import asyncio
-import logging
-
-from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.utils.chat_action import ChatActionMiddleware
+from aiogram import types, Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
+from aiogram.types import CallbackQuery, Message
 
 import config
+from handlers import Gen, kb, utils, parse, rest  # Подключение ваших модулей
 
-from handlers import router 
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-
-
-async def main():
+@dp.callback_query_handler(lambda query: query.data == "generate_rasp")
+async def generate_rasp_callback(query: types.CallbackQuery):
+    await query.answer()
+    await query.message.answer("Давайте начнем! Ответьте на несколько вопросов для составления расписания.")
+    await Gen.text_prompt.set()
     
+@dp.message_handler(state=Gen.text_prompt)
+async def generate_schedule_handler(message: types.Message, state: FSMContext):
+    user_response = message.text
+    df_parse = parse.handle_text(user_response)
+    df_rest = rest.input_data()
+    schedule_text = await utils.generate_schedule(df_parse, df_rest)
+    await message.answer(f"Ваше расписание на день:\n{schedule_text}")
+    await state.finish()
+
+async def main(): 
     try:
         bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
         dp = Dispatcher(storage=MemoryStorage())
@@ -24,9 +36,9 @@ async def main():
     except Exception as e:
         logging.error(f"Ошибка(): {e}")
 
-if __name__ == "__main__" :
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())
+if __name__ == '__main__':
+    from aiogram import executor
+    executor.start_polling(dp, skip_updates=True)
     
 
 
