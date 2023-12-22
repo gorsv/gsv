dbf98d18d7fa15f218140cd7292c2f76a61096c6
FlooGeR31 <82161469+FlooGeR31@users.noreply.github.com>
2023-12-22 19:08:53 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,21 @@ import config
 
 openai.api_key = config.OPENAI_TOKEN
 
+async def generate_schedule(df_parse, df_rest) -> str:
+    try:
+        prompt = f"На основе этих данных составь расписание на день:\n{df_parse}\n{df_rest}"
+        response = await openai.ChatCompletion.acreate(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        return response['choices'][0]['message']['content']
+    except Exception as e:
+        print("Ошибка во время взаимодействия с ChatGPT:", e)
+        return "Извините, произошла ошибка при составлении расписания."
+
 async def generate_text(prompt) -> dict:
     try:
         response = await openai.ChatCompletion.acreate(
