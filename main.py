50cc0421e485209a423ed2226f4ae1150578bbc3
FlooGeR31 <82161469+FlooGeR31@users.noreply.github.com>
2023-12-22 18:41:10 +0300

    Update main.py (try)

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ from handlers import router
 
 async def main():
     
-    bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
-    dp = Dispatcher(storage=MemoryStorage())
-    dp.message.middleware(ChatActionMiddleware())
-    dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    try:
+        bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
+        dp = Dispatcher(storage=MemoryStorage())
+        dp.message.middleware(ChatActionMiddleware())
+        dp.include_router(router)
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(allowed_updates=dp.resolve_used_update_types())
+    except Exception as e:
+        logging.error(f"Ошибка(): {e}")
 
 if __name__ == "__main__" :
     logging.basicConfig(level=logging.INFO)
